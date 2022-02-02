c8c50e0889916f0e3213f853c2d2a191b8cd3f59
Emilia <98789588+emlxya@users.noreply.github.com>
2022-02-01 23:20:13 -0300

    Add files via upload

---
+++ testdehombria.py
@@ -0,0 +1,31 @@
+
+print("Este es un test de revelaciones fuertes, proceder con cuidado")
+input("Presione cualquier tecla para continuar")
+
+estaslisto = input("Estás preparado para entrar? Responde Si o No: ")
+
+if estaslisto == "Si" or \
+        estaslisto == "Sí":
+    print("Bien, responde")
+
+if estaslisto == "No":
+    print("Parece que eres uno de ellos, maricón")
+    exit(
+
+    )
+
+respuesta = input("Tendrías relaciones sexuales con un hombre afeminado? Si o No: ")
+
+if respuesta == "Si" or respuesta == "Sí" or respuesta == "si" or respuesta == "sí" or \
+        respuesta == "SI" or respuesta == "SÍ" or respuesta == "sI" or respuesta == "sÍ":
+    print("Maricón de mierda")
+    exit(
+
+    )
+
+if respuesta == "No" or respuesta == "NO" or \
+        respuesta == "no" or respuesta == "nO":
+    print("Sigue así, gran persona.")
+    exit(
+
+    )
